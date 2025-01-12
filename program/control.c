6967d0d58a1a97bf0c8063d80b3abaf5f9e7eef0
Kazume Nishidate <kazume.nishidate@gmail.com>
2025-01-12 22:19:03 +0900

    reviced

--- program/control.c
+++ program/control.c
@@ -4,141 +4,248 @@
 #include  "md.h"
 #include  "prototypes.h"
 
-/***********************************************************************************/
-//  Silicon (diamond structure) with Stillinger Weber potential
-//
-// REFs.:
-// Takanobu Watanabe, "Molecular Dynamics and Interatomic Potentials",
-// p.102, MORIKITA Syuppann, Tokyo (2023), in Japanese.
-// 
-// F. H. Stillinger and T. A. Weber, "Computer simulation of local
-// order in condensed phases of silicon", Phys. Rev. B., Vol. 31, No. 8, 5262 (1985)
-//
-/***********************************************************************************/
+/*********************************************************************/
+/*  NaCl Born-Mayer-Huggins potential setting                        */
+/*********************************************************************/
+/*****
+*  purpose: system initialisation
+*  all valiables are specified in a MD calculational unit.
+*  see also [manual/units.IEMD]
+*****/
 void get_control_param(void)
 {
-  ctl.calc_max = 400000;  /* maximum MD time step                     */
-  ctl.delta_time_fs = 0.38317;   /* 1 [fs] = 0.001 [ps] = 1.0 X 10^(-15) [s] */
+  ctl.calc_max = 200000;       /* maximum MD time step                       */
+  ctl.delta_time_fs = 3.0;    /* 1 [fs] = 0.001 [ps] = 1.0 X 10^(-15) [s]   */
 
-  ctl.temp = 100.0;       /* Temperature setting [K] */
-  ctl.t_control_step = 5;
-  ctl.set_press_GPa_X = 0.01;  /* Pressure setting  [GPa] */
-  ctl.set_press_GPa_Y = 0.01; 
-  ctl.set_press_GPa_Z = 0.01; 
-  ctl.p_control_step = 20;
+  ctl.temp = 300.0;           /* Temperature setting [K]                    */
+  ctl.t_control_step = 5;     /* scale at every ctl.t_control_step steps    */
+
+  ctl.set_press_GPa_X = 1.0;  /* Pressure setting  [GPa]                    */
+  ctl.set_press_GPa_Y = 1.0; 
+  ctl.set_press_GPa_Z = 1.0; 
+  ctl.p_control_step  =  20;
   sys.pres_scalling_factor = 0.5;
 
-  sys.Ax = 5.42;   /* initial lattice constant [Si with diamond structure] */
-  sys.Ay = 5.42;
-  sys.Az = 5.42;
-  sys.nx = 5;	   /* number of unit cells in x-direction */
+  sys.Ax = 5.63;              /* lattice constant of unit cell [NaCl]       */
+  sys.Ay = 5.63;
+  sys.Az = 5.63;
+  sys.nx = 5;	              /* number of unit cells in x-direction        */
   sys.ny = 5;
   sys.nz = 5;
 
   ctl.natoms_in_unit_cell = 8;     /* number of atoms in unit cell          */
   ctl.natoms_in_mol_unit  = 2;     /* number of atoms in primitive Mol unit */
-  ctl.kinds_of_ions       = 1;     /* Si */
+  ctl.kinds_of_ions       = 2;     /* Na or Cl */
 
-  /* alpha setting for EWALD calculation */
-  sys.a1   = 0.0;   /* to SKIP the EWALD calculation */
-  sys.hm = 20;      /* has no meaning for SW-Si */
-
+  sys.a1     = 0.20375; /* alpha setting for EWALD calculation               */
+  sys.hm     = 25;      /* = |n^2| : cutoff for the reciprocal lattice vector*/
+  sys.radius = 12.0;     /* cutoff radious [A] in real-space                  */
+                        /* sys.radius < Min[MD-basic-cell dimension]/2 [A]   */
 }
 
-/*****
-  identify_ion:
-  every atom is "Si" in Si crystal with diamond structure 
-*****/
 void	identify_ion(void)
 {
   short	x, y, z, i=0;
 
   for(z=0; z<sys.nz*2; z++)
     for(y=0; y<sys.ny*2; y++)
-      for(x=0; x<sys.nx*2; x++){
-	sys.ion[i] = 0;
-	i++;
-      }
+      for(x=0; x<sys.nx*2; x++)
+	sys.ion[i++] = (x%2 + y%2 + z%2)%2;  /* [Na=0 or Cl=1] */
 }
 
 /*****
   void set_loc(void)
-  set up a diamond structure for Si 
+  [NaCl] Set-up the Na and Cl particle locations on NaCl lattice.
 *****/
 void	set_loc(void)
 {
   short	x, y, z, i=0;
   short nx, ny, nz;
-  double nnx=0.0, nny=0.0, nnz=0.0;
-  double nx3=0.0, ny3=0.0, nz3=0.0;
+  double nnx, nny, nnz;
   double shiftx, shifty, shiftz;
 
-  nx = sys.nx * 2;
-  ny = sys.ny * 2;
-  nz = sys.nz * 2;
+  nx  = sys.nx * 2;  ny  = sys.ny * 2;   nz  = sys.nz * 2;
+  nnx = (double)nx;  nny = (double)ny;   nnz = (double)nz; 
 
-/**** 
-  X display shift 
-****/
-  shiftx = (sys.Ax)/2.0;
-  shifty = (sys.Ay)/2.0;
-  shiftz = (sys.Az)/2.0;
-
-/****
-  fcc ion
-****/
+  shiftx = (sys.Lx/nnx)/2.0;
+  shifty = (sys.Ly/nny)/2.0;
+  shiftz = (sys.Lz/nnz)/2.0;
 
   for(z=0; z<nz; z++)
     for(y=0; y<ny; y++)
-      for(x=0; x<nx; x++)
-	{
-	  if((x+y+z)%2==0){
-	    nnx = (double)(x)/2.0;
-	    nny = (double)(y)/2.0;
-	    nnz = (double)(z)/2.0;
-	    sys.rx[i] = sys.Ax * nnx +shiftx;
-	    sys.ry[i] = sys.Ay * nny +shifty;
-	    sys.rz[i] = sys.Az * nnz +shiftz;
-	    i++;
-
-	    nx3 = nnx + 0.25;
-	    ny3 = nny + 0.25;
-	    nz3 = nnz + 0.25;
-	    sys.rx[i] = sys.Ax * nx3 +shiftx;
-	    sys.ry[i] = sys.Ay * ny3 +shifty;
-	    sys.rz[i] = sys.Az * nz3 +shiftz;
-	    i++;
-	  }
-	}
+      for(x=0; x<nx; x++) {
+	sys.rx[i] = sys.Lx*(double)(x/nnx)+shiftx;
+	sys.ry[i] = sys.Ly*(double)(y/nny)+shifty;
+	sys.rz[i] = sys.Lz*(double)(z/nnz)+shiftz;
+	i++;
+      }
 }
 
+/*----------------------- Born-Mayer-Huggins potential [NaCl] ------------- */
 void set_potential(void)
 {
-  double m_Si;
+  double m_na, m_cl, z_na, z_cl;
 
-  m_Si = 28.0855 /(6.0221367 * 1.6605402 / 10.0); /* [m_u] */
+  /* --------------------------------------------------------------- */
+  /*   Huggins-Mayer potential with Tosi-Fumi parameter set          */
+  /*   see references for more detail                                */
+  /* --------------------------------------------------------------- */
+  /*                                                                 */
+  /*  Function Form :                                                */
+  /* u_(i,j) = Z_(i)*Z_(j)*e^(2)/r_(i,j) +                           */
+  /*     c_(i,j)*b*Exp((sigma_(i) + sigma_(j) - r_(i,j))/rho)        */
+  /*         - ( C_(i,j)/(r_(i,j)^(6)) ) - ( D_(i,j)/(r_(i,j)^(8)) ) */
+  /*                                                                 */
+  /*  Parameter Table :                                              */
+  /*    b = 3.38e-13 [erg]                                           */
+  /*                                                                 */
+  /*    c_na_na = 1.25                                               */
+  /*    c_na_cl = 1                                                  */
+  /*    c_cl_cl = 0.75                                               */
+  /*                                                                 */
+  /*    sigma_na = 1.170 [A]                                         */
+  /*    sigma_cl = 1.585 [A]                                         */
+  /*    rho      = 0.317 [A]                                         */
+  /*                                                                 */
+  /*    C_na_na  = 1.68e-60  [erg][cm^6]                             */
+  /*    C_na_cl  = 11.2e-60  [erg][cm^6]                             */
+  /*    C_cl_cl  = 116.0e-60 [erg][cm^6]                             */
+  /*                                                                 */
+  /*    D_na_na  = 0.8e-76   [erg][cm^8]                             */
+  /*    D_na_cl  = 13.9e-76  [erg][cm^8]                             */
+  /*    D_cl_cl  = 233.0e-76 [erg][cm^8]                             */
+  /*                                                                 */
+  /*                 |                                               */
+  /*                 V                                               */
+  /*        UNIT  CONVERTED  [erg]->[energy],[cm]->[A]               */
+  /*                 |                                               */
+  /*                 V                                               */
+  /*                                                                 */
+  /*----------- UNIT CONVERSION  ------------------------------------*/
+  /*  1[erg] = 1.0e-7 [J]                                            */
+  /*                                                                 */
+  /*  b = 3.38e-13 [erg]                                             */
+  /*                                                                 */
+  /*    =  3.383e-13 * 1.0e-7 [J]                                    */
+  /*                                                                 */
+  /*       3.38e-13 * 1.0e-07 [J]              [energy]              */
+  /*    = ------------------------  *   1.0e18 ----------            */
+  /*                1                           [ J ]                */
+  /*    = 3.38e-2 [energy]                                           */
+  /*                                                                 */
+  /*-----------------------------------------------------------------*/
+  /*                                                                 */
+  /*    b = 3.38e-2 [energy]                                         */
+  /*                                                                 */
+  /*    c_na_na = 1.25                                               */
+  /*    c_na_cl = 1                                                  */
+  /*    c_cl_cl = 0.75                                               */
+  /*                                                                 */
+  /*    sigma_na = 1.170  [A]                                        */
+  /*    sigma_cl = 1.585  [A]                                        */
+  /*    rho      = 0.317  [A]                                        */
+  /*                                                                 */
+  /*    C_na_na  = 1.68e-01  [energy][A]                             */
+  /*    C_na_cl  = 11.2e-01  [energy][A]                             */
+  /*    C_cl_cl  = 116.0e-01 [energy][A]                             */
+  /*                                                                 */
+  /*    D_na_na  = 0.8e-01   [energy][A]                             */
+  /*    D_na_cl  = 13.9e-01  [energy][A]                             */
+  /*    D_cl_cl  = 233.0e-01 [energy][A]                             */
+  /*                                                                 */
+  /* ----------------------------------------------------------------*/
 
-  ion.m[0] = m_Si;
+  z_na =  1.0 * sqrt(sys.kk);
+  z_cl = -1.0 * sqrt(sys.kk);
+  m_na = 22.9898 /(6.0221367 * 1.6605402 / 10.0); /* [m_u] */
+  m_cl = 35.4528 /(6.0221367 * 1.6605402 / 10.0); /* [m_u] */
 
-  /* ------------ S.W Potential Set --------------*/
-  sw.ep = (50.0* 0.04337) * 1.60217733e-1;  /* [eV -> energy] */
-  sw.sig = 2.0951;                 /* [A] */
-  sw.A = 7.049556277*sw.ep;        /* [energy]       */
-  sw.B = 0.6022245584*(sw.sig*sw.sig*sw.sig*sw.sig);
-  sw.a = 1.8*sw.sig;               /* cut-off radius */
-  sw.lam = 21.0*sw.ep;
-  sw.gam = 1.2*sw.sig;
-  sw.beta = -2.0*sw.ep*sw.lam;
-  /* ---------------------------------------------*/
-
+  ion.Z[0] = z_na;
+  ion.m[0] = m_na;
+  ion.Z[1] = z_cl;
+  ion.m[1] = m_cl;
+  
+  hm.b = 3.38/100.0;
+  hm.c_na_na = 1.25;
+  hm.c_na_cl = 1.0;
+  hm.c_cl_cl = 0.75;
+  hm.sigma_na = 1.170;
+  hm.sigma_cl = 1.585;
+  hm.rho      = 0.317;
+  hm.C_na_na  = 1.68/10.0;
+  hm.C_na_cl  = 11.2/10.0;
+  hm.C_cl_cl  = 116.0/10.0;
+  hm.D_na_na  = 0.8/10.0;
+  hm.D_na_cl  = 13.9/10.0;
+  hm.D_cl_cl  = 233.0/10.0; 
 }
 
-void mk_table(void)
+void mk_table(void)   /* make a look up table for BMH potential */
 {
-  /*  Silicon (diamond structure) with Stillinger Weber potential  */
-  /*  this function do nothing.  see "real.c".                     */
-}
+  int ion_i, ion_j, ddr;
+  int ij = 0, iijj;
+  double dr, dr6, dr8, adr, erfc_adr_per_dr, ZZ;
+  double hm_c_ij, hm_sigma_ij, hm_C_ij, hm_D_ij;
+  double p_e1, p_r1, f_e1, f_r1;
 
+  for(ion_i=0;ion_i<ctl.kinds_of_ions;ion_i++) {
+    for(ion_j=ion_i;ion_j<ctl.kinds_of_ions;ion_j++) {
+
+      iijj = ion_i + ion_j;
+
+      switch (iijj) {
+      case 0:
+        hm_c_ij = hm.c_na_na;
+        hm_sigma_ij = hm.sigma_na + hm.sigma_na;
+        hm_C_ij = hm.C_na_na;
+        hm_D_ij = hm.D_na_na;
+        break;
+      case 1:
+        hm_c_ij = hm.c_na_cl;
+        hm_sigma_ij = hm.sigma_na + hm.sigma_cl;
+        hm_C_ij = hm.C_na_cl;
+        hm_D_ij = hm.D_na_cl;
+        break;
+      default:
+        hm_c_ij = hm.c_cl_cl;
+        hm_sigma_ij = hm.sigma_cl + hm.sigma_cl;
+        hm_C_ij = hm.C_cl_cl;
+        hm_D_ij = hm.D_cl_cl;
+        break;
+      }
+
+      ZZ = ion.Z[ion_i] * ion.Z[ion_j];
+
+      for(ddr=0;ddr<sys.table_division+1;ddr++) {  /* division = 0.001 [A] */
+
+	dr = ((double)ddr)/(FP_ARRAY) + 0.5; /* 0.5 [A] to sys.radius (cut-off) */
+	dr6 = dr*dr*dr*dr*dr*dr;
+	dr8 = dr6*dr*dr;
+	adr = sys.a1 * dr;  
+	erfc_adr_per_dr = erfcc(adr)/dr;
+
+	/* Potential */
+	p_e1 = ZZ * erfc_adr_per_dr;
+	p_r1 = hm_c_ij*hm.b*exp((hm_sigma_ij - dr)/hm.rho) -
+	  hm_C_ij/dr6 - hm_D_ij/dr8;
+
+	/* Force */
+	f_e1 = ZZ* (2.0 * sys.a3 * exp(- adr * adr) +
+		    erfc_adr_per_dr )/(dr*dr);
+	f_r1 = (hm_c_ij*hm.b*exp((hm_sigma_ij - dr)/hm.rho)/(hm.rho) -
+		6.0*hm_C_ij/(dr6*dr) - 8.0*hm_D_ij/(dr8*dr))/dr;
+
+	sys.pe1r1[ij*(sys.table_division+1)+ddr] = p_e1 + p_r1;
+	sys.fe1r1[ij*(sys.table_division+1)+ddr] = f_e1 + f_r1;
+      }
+
+      /* fill the lookup table index */
+      sys.lookup[ctl.kinds_of_ions * ion_i + ion_j] = ij;
+      sys.lookup[ctl.kinds_of_ions * ion_j + ion_i] = ij;
+      ij++;  /* shift the look-up table column */
+    }
+  }
+}
 
 void   md_xyz(void)
 {
@@ -150,8 +257,14 @@ void   md_xyz(void)
   fprintf(fpmdxyz,"0.0 0.0 %3.6f\" ",sys.Lz);
   fprintf(fpmdxyz,"Properties=species:S:1:pos:R:3 %6d\n",sys.step);    
   for(i=0;i<sys.N;i++) { /* [A] unit */
-    fprintf(fpmdxyz,"Si ");
-  }
+    if(sys.ion[i]==0) {
+      fprintf(fpmdxyz,"Na ");
+    } else {fprintf(fpmdxyz,"Cl ");
+    }
     fprintf(fpmdxyz," %3.6f   %3.6f   %3.6f \n", sys.rx[i],sys.ry[i],sys.rz[i]);
+  }
 }
+
+
+
 
--- program/real.c
+++ program/real.c
@@ -7,160 +7,78 @@
 /*****
   void	real_space(void)
 
-  The function for potential and force calculation of Si diamond structure
-  with Stillinger Weber potentail set.
+  The function for potential and force calculation of
+  <Ewald-first-term> + <repulsion> terms using the look-up table
+  created by mk_table() in control.c.
 
 *****/
 void	real_space(void)
 {
-  int i, j, k; 
+  int i, j, ij, ddr;
+  double dr;
+  double pij, fij;
+  double dx, dy, dz;
   double rdx, rdy, rdz;
-  double x_ij, y_ij, z_ij;
-  double x_ik, y_ik, z_ik;
-  double x_jk, y_jk, z_jk;
-  double r_ij, r_ik, r_jk;
-
-  double r_ij4;
-
-  double for_2, f2_1, f2_2;
-  double cs_jik;
-  double ex_jik;
-
-  double fa_jik, fb_jik, fd_jik;
+  double rxi, ryi, rzi;
 
   rdx = sys.Lx*0.5;
   rdy = sys.Ly*0.5;
   rdz = sys.Lz*0.5;
 
-  /* initialization */
   sys.pot = 0.0;
-  sys.virX = 0.0;  sys.virY = 0.0;  sys.virZ = 0.0;
-  
+  sys.virX = 0.0;
+  sys.virY = 0.0;
+  sys.virZ = 0.0;
+
   for(i=0; i<sys.N; i++) {
-    sys.fx[i] = 0.0; sys.fy[i] = 0.0; sys.fz[i] = 0.0;
-  }
-  
-  for(i=0; i<sys.N; i++) {
+    rxi = sys.rx[i];
+    ryi = sys.ry[i];
+    rzi = sys.rz[i];
+
     for(j=i+1; j<sys.N; j++) {
-      x_ij = sys.rx[i] - sys.rx[j];
-      y_ij = sys.ry[i] - sys.ry[j];
-      z_ij = sys.rz[i] - sys.rz[j];
-      
+      dx = rxi - sys.rx[j];
+      dy = ryi - sys.ry[j];
+      dz = rzi - sys.rz[j];
+
       /* cyclic boundary condition */
-      if(x_ij > rdx) x_ij -= sys.Lx;
-      if(y_ij > rdy) y_ij -= sys.Ly;
-      if(z_ij > rdz) z_ij -= sys.Lz;
-      if(x_ij < -rdx) x_ij += sys.Lx;
-      if(y_ij < -rdy) y_ij += sys.Ly;
-      if(z_ij < -rdz) z_ij += sys.Lz;
+      if(dx > rdx) dx -= sys.Lx;
+      if(dy > rdy) dy -= sys.Ly;
+      if(dz > rdz) dz -= sys.Lz;
+      if(dx < -rdx) dx += sys.Lx;
+      if(dy < -rdy) dy += sys.Ly;
+      if(dz < -rdz) dz += sys.Lz;
 
-      /*  ion_i - ion_j distance  */
-      r_ij = sqrt(x_ij*x_ij + y_ij*y_ij + z_ij*z_ij);
-      
+      /* dr' = dr^2 */
+      dr = dx*dx + dy*dy + dz*dz;
+
       /* sys.radius = cut-off radius */
-      if(r_ij >= sw.a) continue;
+      /* sys.radius2 = (cut-off radius)^2 */
+      if(dr > sys.radius2) continue; 
 
-      r_ij4 = r_ij * r_ij * r_ij * r_ij;
-      
-      /* ----------------  SET 2-BODY POTENTIAL ---------------------*/
-      sys.pot += sw.ep*sw.A*(sw.B/r_ij4-1.0)*exp(1.0/(r_ij-sw.a));
-      
-      /* ----------------  SET 2-BODY FORCE ------------------------*/
-      f2_1 = -sw.ep*sw.A*exp(1.0/(r_ij-sw.a))/r_ij;
-      f2_2 = -(4.0*sw.B/(r_ij4*r_ij)) + (1.0-sw.B/r_ij4)/((r_ij-sw.a)*(r_ij-sw.a));
-      for_2 = f2_1 * f2_2;
+      /* shift the look-up table column */
+      ij = sys.lookup[sys.ion[i]*(ctl.kinds_of_ions) + sys.ion[j]];
 
-      sys.fx[i] += (for_2 * x_ij);
-      sys.fy[i] += (for_2 * y_ij);
-      sys.fz[i] += (for_2 * z_ij);
+      /* dr' -> sqrt(dr) */
+      ddr = (int)((sqrt(dr)-0.5)*(FP_ARRAY));
 
-      sys.fx[j] -= (for_2 * x_ij);
-      sys.fy[j] -= (for_2 * y_ij);
-      sys.fz[j] -= (for_2 * z_ij);
+      /* look-up the table */
+      pij = *(sys.pe1r1+ij*(sys.table_division+1)+ddr);
+      fij = *(sys.fe1r1+ij*(sys.table_division+1)+ddr);
 
-      /* VIRIAL */
-      sys.virX += (for_2 * x_ij) * x_ij; 
-      sys.virY += (for_2 * y_ij) * y_ij; 
-      sys.virZ += (for_2 * z_ij) * z_ij; 
-    }
-  }
+      sys.pot += pij;
 
-  /* ------------------  SET 3-BODY ------------------------------*/
-  for(i=0;i<sys.N;i++)  {
-    for(j=0;j<sys.N;j++)  {
-      if(i==j) continue;
-      x_ij = sys.rx[i] - sys.rx[j];
-      y_ij = sys.ry[i] - sys.ry[j];
-      z_ij = sys.rz[i] - sys.rz[j];
-      
-      /* cyclic boundary condition */
-      if(x_ij > rdx) x_ij -= sys.Lx;
-      if(y_ij > rdy) y_ij -= sys.Ly;
-      if(z_ij > rdz) z_ij -= sys.Lz;
-      if(x_ij < -rdx) x_ij += sys.Lx;
-      if(y_ij < -rdy) y_ij += sys.Ly;
-      if(z_ij < -rdz) z_ij += sys.Lz;
+      sys.fx[i] += fij * dx;
+      sys.fy[i] += fij * dy;
+      sys.fz[i] += fij * dz;
+      sys.fx[j] -= fij * dx;
+      sys.fy[j] -= fij * dy;
+      sys.fz[j] -= fij * dz;
+      /* (note): fij' => -Fij/dr, fijx => -fij'*dx => -Fij*dx/dr  */
 
-      /*  ion_i - ion_j distance  */
-      r_ij = sqrt(x_ij*x_ij + y_ij*y_ij + z_ij*z_ij);
-      
-      /* sys.radius = cut-off radius */
-      if(r_ij >= sw.a) continue;
-
-      for(k=j+1;k<sys.N;k++){
-	if(k==i) continue;
-	x_ik = sys.rx[i] - sys.rx[k];  
-	y_ik = sys.ry[i] - sys.ry[k];  
-	z_ik = sys.rz[i] - sys.rz[k];  
-	
-	/* cyclic boundary condition */
-	if(x_ik > rdx) x_ik -= sys.Lx;
-	if(y_ik > rdy) y_ik -= sys.Ly;
-	if(z_ik > rdz) z_ik -= sys.Lz;
-	if(x_ik < -rdx) x_ik += sys.Lx;
-	if(y_ik < -rdy) y_ik += sys.Ly;
-	if(z_ik < -rdz) z_ik += sys.Lz;
-	
-	/* ion_i - ion_k distance */
-	r_ik = sqrt(x_ik*x_ik + y_ik*y_ik + z_ik*z_ik);
- 	
-	if(r_ik > sw.a) continue;
-	
-	x_jk = x_ik - x_ij;
-	y_jk = y_ik - y_ij;
-	z_jk = z_ik - z_ij;	
-
-	r_jk = sqrt(x_jk*x_jk + y_jk*y_jk + z_jk*z_jk);	
-	cs_jik = (x_ij*x_ik + y_ij*y_ik + z_ij*z_ik)/(r_ij * r_ik);
-	ex_jik = exp(sw.gam/(r_ij-sw.a)+sw.gam/(r_ik-sw.a));
-
-	sys.pot += sw.lam*ex_jik*(cs_jik+1.0/3.0)*(cs_jik+1.0/3.0);
-
-	fa_jik = -sw.lam*ex_jik
-	  *(cs_jik+1.0/3.0)*((cs_jik+1.0/3.0)*sw.gam/(r_ij-sw.a)/(r_ij-sw.a)
-			    -2.0*(r_ij-r_ik*cs_jik)/r_ij/r_ik);
-
-	fb_jik = -sw.lam*ex_jik
-	  *(cs_jik+1.0/3.0)*((cs_jik+1.0/3.0)*sw.gam/(r_ik-sw.a)/(r_ik-sw.a)
-			    -2.0*(r_ik-r_ij*cs_jik)/r_ik/r_ij);
-
-	fd_jik = -sw.lam*exp(sw.gam/(r_ij-sw.a)+sw.gam/(r_ik-sw.a))
-	  *(cs_jik+1.0/3.0)*2.0*r_jk/r_ij/r_ik;
-
-	sys.fx[i] += -fa_jik*x_ij/r_ij - fb_jik*x_ik/r_ik;
-	sys.fy[i] += -fa_jik*y_ij/r_ij - fb_jik*y_ik/r_ik;
-	sys.fz[i] += -fa_jik*z_ij/r_ij - fb_jik*z_ik/r_ik;
-
-	sys.fx[j] +=  fa_jik*x_ij/r_ij - fd_jik*x_jk/r_jk;
-	sys.fy[j] +=  fa_jik*y_ij/r_ij - fd_jik*y_jk/r_jk;
-	sys.fz[j] +=  fa_jik*z_ij/r_ij - fd_jik*z_jk/r_jk;
-
-	sys.fx[k] +=  fb_jik*x_ik/r_ik + fd_jik*x_jk/r_jk;
-	sys.fy[k] +=  fb_jik*y_ik/r_ik + fd_jik*y_jk/r_jk;
-	sys.fz[k] +=  fb_jik*z_ik/r_ik + fd_jik*z_jk/r_jk;
-	
-      }
-    }
+      /* VIRIAL : (repulsion) + (EWALD 1st term) */
+      sys.virX += fij * dx * dx; 
+      sys.virY += fij * dy * dy; 
+      sys.virZ += fij * dz * dz; 
+    }	     
   }
 }
-
